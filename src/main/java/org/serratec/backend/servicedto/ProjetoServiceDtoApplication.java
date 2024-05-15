cdfb2c3d52bf9c51200b6d23d267fe1b7f7fbb10
LAPTOP-TN11MPJB\alberto.paz <alberto.paz@neki-it.com.br>
2024-05-15 20:15:15 -0300

    feat: criando estrutura inicial do projeto

---
+++ src/main/java/org/serratec/backend/servicedto/ProjetoServiceDtoApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.servicedto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoServiceDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoServiceDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/backend/servicedto/ProjetoServiceDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.servicedto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoServiceDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
