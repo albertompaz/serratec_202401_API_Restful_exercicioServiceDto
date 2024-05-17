915fe543acb831e1f6b0b708f6e95b6b024ce8ef
LAPTOP-TN11MPJB\alberto.paz <alberto.paz@neki-it.com.br>
2024-05-17 18:53:22 -0300

    feat: adiciona migração para criar funcionario

---
+++ src/main/resources/db/migration/V03__criar_tabela_funcionario.sql
@@ -0,0 +1,38 @@
+CREATE TABLE funcionario (
+id_funcionario serial PRIMARY KEY,
+nome varchar(60),
+data_nascimento date,
+salario NUMERIC
+);
+
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Carlos','2000-05-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('João','1999-06-11',2000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Arthur','1998-02-11',3550);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Marcos','2001-02-22',4200);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Adriana','1998-01-22',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Carlos','1974-11-21',2500);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Carlos','1966-08-12',1950);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Yure','1955-02-10',10000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Ana Beatriz','1969-06-11',2300);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Liliane','1974-11-10',3200);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Liliam','2000-01-22',6500);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Mariana','2001-04-28',2500);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Maria José','1978-02-22',2200);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Carlos André','1976-12-22',2300);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('CArlos Arthur','1988-08-12',3800);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Carlos Rodrigues','1989-02-12',2800);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Lucas','2001-05-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Roni','2002-05-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Reinaldo','2003-06-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Silvio','2004-05-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Sergio','2000-02-11',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Silas','2000-01-10',2000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Juarez','1988-05-10',4000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Flávio','1978-05-10',5000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Augusto','1977-06-10',1000);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Rômulo','1988-04-10',1400);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Adriana','2000-01-10',1040);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Maiara','2001-03-10',1040);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Paulo','1999-02-10',1050);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Beatriz','1976-01-10',1500);
+INSERT INTO funcionario(nome, data_nascimento, salario) VALUES ('Laura','1977-03-10',1300);
